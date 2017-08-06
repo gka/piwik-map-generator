616a6d7b5cf495fbb22a622dc4716671d542c995
Marc Neudert <marc.neudert@gmail.com>
2017-08-06 20:36:51 +0200

    allows smooth re-download of shapefiles

--- shp/download.sh
+++ shp/download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip
-unzip -d . ne_50m_admin_0_countries.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/physical/ne_50m_lakes.zip
-unzip -d . ne_50m_lakes.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip
-unzip -d . ne_10m_admin_1_states_provinces.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip -O ne_50m_admin_0_countries.zip
+unzip -j -o -d . ne_50m_admin_0_countries.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/physical/ne_50m_lakes.zip -O ne_50m_lakes.zip
+unzip -j -o -d . ne_50m_lakes.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip -O ne_10m_admin_1_states_provinces.zip
+unzip -j -o -d . ne_10m_admin_1_states_provinces.zip
